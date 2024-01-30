d6306aef0d8239c97e003ba458d51f1e29934318
jordan Jimenez <jordan.jimenez@ecole-isitech.fr>
2024-01-30 17:57:22 +0100

    Modif layout - import global.css

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,10 +1,12 @@
+import '@/app/ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body>{children}</body>
     </html>
   );
