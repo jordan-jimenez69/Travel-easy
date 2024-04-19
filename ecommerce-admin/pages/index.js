3ae67c4188ac9e15f09462c04f0d5f501292f40c
jordan Jimenez <jordan.jimenez@ecole-isitech.fr>
2024-04-19 12:54:16 +0200

    ajout page categories et parametres

--- ecommerce-admin/pages/index.js
+++ ecommerce-admin/pages/index.js
@@ -1,8 +1,13 @@
 import Layout from "@/components/layout";
-
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const {data: session} = useSession();
   return <Layout>
-    test
+    <div className="text-orange-400 flex">
+      <h2>
+      hello, {session?.user?.name}
+      </h2>
+    </div>
   </Layout>
 }
