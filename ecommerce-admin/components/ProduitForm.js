26edbc80949f87ec0b1313b015d47b878c3d7bb3
jordan Jimenez <jordan.jimenez@ecole-isitech.fr>
2024-04-25 21:49:42 +0200

    upload image update

--- ecommerce-admin/components/ProduitForm.js
+++ ecommerce-admin/components/ProduitForm.js
@@ -8,7 +8,8 @@ export default function ProduitForm({
     description: existingdescription,
     price: existingprice,
     level: existinglevel,
-    saison: existingsaison
+    saison: existingsaison,
+    images
 }) {
     const [title, setTitle] = useState(existingtitle || '');
     const [level, setLevel] = useState(existinglevel || '');
@@ -34,6 +35,18 @@ export default function ProduitForm({
         router.push('/produits');
     }
 
+    async function uploadImages(ev) {
+        const files = ev.target?.files;
+        if (files?.length > 0) {
+            const data = new FormData();
+            for (const file of files) {
+                data.append('file', file);
+            }
+            const res = await axios.post('/api/upload/', data);
+            console.log(res.data);
+        }
+    }
+
     return (
         <form onSubmit={saveProduct}>
 
@@ -45,6 +58,28 @@ export default function ProduitForm({
                     value={title}
                     onChange={ev => setTitle(ev.target.value)}
                 />
+            </div>
+
+            <div className="form-group-strict">
+                <label>Photo</label>
+                <div className="mt-2">
+                    <label className="w-24 h-24 border flex text-center cursor-pointer items-center justify-center text-sm gap-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-upload" viewBox="0 0 16 16">
+                            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
+                            <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z" />
+                        </svg>
+                        <div>upload</div>
+                        <input
+                            type="file"
+                            className="hidden"
+                            onChange={uploadImages} />
+                    </label>
+                    {!images?.length && (
+                        <div>
+                            Pas de Photo
+                        </div>
+                    )}
+                </div>
             </div>
 
             <div className="form-group">
