3a0c3edf1f30a026f2e363534e6d428093fdafb5
jordan Jimenez <jordan.jimenez@ecole-isitech.fr>
2024-05-23 18:05:12 +0200

    Page Home effectuer

--- ecommerce-front/pages/_app.js
+++ ecommerce-front/pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import "@/styles/navbar.css";
+import "@/styles/accueil.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- ecommerce-front/pages/index.js
+++ ecommerce-front/pages/index.js
@@ -1,7 +1,13 @@
+import Accueil from "@/composants/accueil";
 import Navbar from "@/composants/navbar";
 
 export default function Home() {
   return (
+    <>
     <Navbar />
+    
+    <Accueil />
+
+    </>
 );
 }
