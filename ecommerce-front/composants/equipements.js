87b9c057dff492bf9b932f6a242b6cb1ee03978e
jordan Jimenez <jordan.jimenez@ecole-isitech.fr>
2024-05-30 18:55:29 +0200

    update equipement

--- ecommerce-front/composants/equipements.js
+++ ecommerce-front/composants/equipements.js
@@ -30,16 +30,31 @@ export default function Equipement() {
 
                 </section>
 
-                <div className="voir-plus">
-                    <Link className="text-voirplus" href={""}>
-                        Voir plus d'equipements
-                        <div className="fleche-voirplus">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
-                            </svg>
-                        </div>
-                    </Link>
-                </div>
+                <section className="list-equipement">
+                    <div className="equipement">
+                        <img className="img-list-equip" src="lit.jpg" />
+                        <Link className="btn-equipement" href={""}>Acheter maintenant</Link>
+                    </div>
+
+                    <div className="equipement">
+                        <img className="img-list-equip" src="blouson.jpg" />
+                        <Link className="btn-equipement" href={""}>Acheter maintenant</Link>
+                    </div>
+
+                    <div className="equipement voir-plus">
+                        <h1 className="text-equip-plus">Accédez à notre <span>catalogue</span> complet ici !</h1>
+                        <Link className="text-voirplus" href={""}>
+                            Découvrez tout les équipements
+                            <div className="fleche-voirplus">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                                </svg>
+                            </div>
+                        </Link>
+                    </div>
+                </section>
+
+    
 
             </div>
         </>
