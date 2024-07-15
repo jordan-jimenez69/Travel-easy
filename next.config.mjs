b7b85ed6fbe5cd07bdc76eeba5ae7a7afed90b10
Jordan JIMENEZ <jordan.jimenez@ecole-isitech.fr>
2024-07-15 14:29:19 +0200

    merge2

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/Travel-easy',
+  assetPrefix: '/Travel-easy',
   reactStrictMode: true,
   async rewrites() {
     return [
